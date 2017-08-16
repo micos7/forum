bddf64829bc0a4e6aeb4a35c07a9e1e25565c441
micos7 <micos7@gmail.com>
2017-08-16 08:40:34 +0300

    Reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,5 +22,20 @@
             @endforeach
         </div>
     </div>
+
+    @if(auth()->check())
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <form action="{{ $thread->path(). '/replies'}}" method="POST">
+            {{csrf_field()}}
+                <textarea name="body" id=body"" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+            <button type="submit" class="btn btn-default">Post</button>
+            </form>
+        </div>
+    </div>
+    @else
+    <p class="text-center">Please <a href="{{route('login')}}">sign in</a></p>
+    @endif
+
 </div>
 @endsection
