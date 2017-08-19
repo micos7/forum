172bd4230b01f8825df1539b71919b47bc21d487
micos7 <micos7@gmail.com>
2017-08-19 19:19:28 +0300

    Create thread form and exception handling

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -28,4 +28,11 @@ class CreateThreadsTest extends TestCase
         
         $this->post('/threads', $thread->toArray());
     }
+
+    /** @test */
+    function guests_cannot_see_the_create_thread_page()
+    {
+        $this->withExceptionHandling()->get('/threads/create')->assertRedirect('/login');
+    }
+
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace Tests;
-
+use App\Exceptions\Handler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->disableExceptionHandling();
+    }
 
     protected function signIn($user=null)
     {
@@ -14,6 +21,25 @@ abstract class TestCase extends BaseTestCase
 
         $this->actingAs($user);
 
+        return $this;
+    }
+
+    // Hat tip, @adamwathan.
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(\Exception $e) {}
+            public function render($request, \Exception $e) {
+                throw $e;
+            }
+        });
+    }
+
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
         return $this;
     }
 }
