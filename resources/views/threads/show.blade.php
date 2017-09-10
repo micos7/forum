f2e02426a26613f7288f2d00004fcaee0c7ea3d9
micos7 <micos7@gmail.com>
2017-09-10 19:22:29 +0300

    Meta information,pagination

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,39 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
                 <a href="#"> {{$thread->creator->name}} </a> posted:
-                {{ $thread->title }}</div>
+                {{ $thread->title }}
+                </div>
 
                 <div class="panel-body">
                     {{$thread->body}}
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-        @foreach($thread->replies as $reply)
-            @include('threads.reply')
+            
+            @foreach($replies as $reply)
+                @include('threads.reply')
             @endforeach
-        </div>
-    </div>
 
-    @if(auth()->check())
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+            {{$replies->links()}}
+
+            @if(auth()->check())
+
             <form action="{{ $thread->path(). '/replies'}}" method="POST">
             {{csrf_field()}}
                 <textarea name="body" id=body"" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
             <button type="submit" class="btn btn-default">Post</button>
             </form>
-        </div>
-    </div>
+
     @else
     <p class="text-center">Please <a href="{{route('login')}}">sign in</a></p>
     @endif
 
+        
+    </div>
+    <div class="col-md-4">
+            <div class="panel panel-default">
+                
+
+                <div class="panel-body">
+                    <p>This thread was published {{$thread->created_at->diffForHumans()}}
+                    by <a href="#">{{$thread->creator->name}}</a> 
+                    and currently has {{ $thread->replies()->count()}} {{ str_plural('comment',$thread->replies()->count())}}.
+                    </p> 
+                </div>
+            </div>
+            </div>
+    </div>
 </div>
 @endsection
