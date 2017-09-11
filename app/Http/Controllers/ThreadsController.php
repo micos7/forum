abf54c796c8ca6e228bcfe33309d891fcbcde888
micos7 <micos7@gmail.com>
2017-09-11 23:15:58 +0300

    Ref

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -14,18 +14,15 @@ class ThreadsController extends Controller
     {
         $this->middleware('auth')->except(['index','show']);
     }
-    private function getThreads(Channel $channel){
-        if($channel->exists){
-            $threads = $channel->threads()->latest();
-        }else{
-            $threads= Thread::latest();
-        }
-
-        if($username = request('by')){
-            $user = \App\User::where('name', $username)->firstOrFail();
-            $threads->where('user_id',$user->id);
-        }
-        $threads = $threads->get();
+    private function getThreads(Channel $channel,ThreadFilters $filters){
+        $threads = Thread::latest()->filter($filters);
+        
+                if($channel->exists){
+                    $threads->where('channel_id',$channel->id); 
+                }
+                
+                
+                $threads = $threads->get();
         return $threads;
     }
     /**
@@ -35,15 +32,8 @@ class ThreadsController extends Controller
      */
     public function index(Channel $channel,ThreadFilters $filters)
     {
-        $threads = Thread::latest()->filter($filters);
+        $threads =$this->getThreads($channel,$filters);
 
-        if($channel->exists){
-            $threads->where('channel_id',$channel->id); 
-        }
-        
-        
-        $threads = $threads->get();
-        
         return view('threads.index',compact('threads'));
     }
 
